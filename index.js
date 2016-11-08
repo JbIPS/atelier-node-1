65d493b3d0c19938424da359d7e853c4ecbb7102
JbIPS <jeanbaptiste.richardet@gmail.com>
2016-11-08 15:42:53 +0100

    Final

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 'use strict';
 
 const Http = require('http');
+const request = require('request');
 const RequestTransformer = require('./transformer.js');
+
+const randomWords = ['world', 'Node.js', 'beautiful', 'Silex Labs'];
 
 // Crée un serveur HTTP
 // Le serveur écoute sur le port défini dans l'environement ou le 8989 par défault
@@ -12,7 +15,12 @@ console.log(`Le serveur est prêt et écoute sur le port ${PORT}`);
 // Fonction a exécuter à chaque requête reçue
 server.on('request', (req, res) => {
   if(req.method == 'POST') {
-    req.pipe(new RequestTransformer()).pipe(res);
+    // On envoie le contenu de la requête au transformer
+    let stream = req.pipe(new RequestTransformer(null, randomWords));
+    // On passe le tout au suivant
+    if(req.url !== '/') stream = stream.pipe(request.post(req.url.substr(1)));
+    // On oublie pas d'envoyer le resultat au client
+    stream.pipe(res);
   } else {
     res.end('Je ne sais rien faire en GET, mais envoyez moi plutôt des données en POST :)');
   }
--- transformer.js
+++ transformer.js
@@ -3,12 +3,15 @@
 const Transform = require('stream').Transform;
 
 class RequestTransformer extends Transform {
-  constructor(options) {
+  constructor(options, dictionary) {
     super(options);
+    this.dictionary = dictionary;
   }
 
   _transform(chunk, encoding, callback) {
-    callback(null, chunk);
+    const input = chunk.toString();
+    const randomWord = this.dictionary[Math.floor(Math.random() * this.dictionary.length)];
+    callback(null, `${input} ${randomWord}`);
   }
 
   _flush(callback) {
